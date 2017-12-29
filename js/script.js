f6bda11b6d8ba0bd24f26b9e5f4dc1497930b266
Pierre <p.clavequin@yahoo.fr>
2017-12-30 01:45:23 +0800

    Update data

--- js/script.js
+++ js/script.js
@@ -5,13 +5,13 @@
 $(function () {
   // data stolen from http://howmanyleft.co.uk/vehicle/jaguar_'e'_type
   var tax_data = [
-       {"period": "2011 Q4", "licensed": 3407, "sorned": 660},
-       {"period": "2010 Q4", "licensed": 3246, "sorned": 661},
-       {"period": "2009 Q4", "licensed": 3171, "sorned": 676},
-       {"period": "2008 Q4", "licensed": 3155, "sorned": 681},
-       {"period": "2007 Q4", "licensed": 2726, "sorned": 620},
-       {"period": "2006 Q4", "licensed": 2245, "sorned": null},
-       {"period": "2005 Q4", "licensed": 1289, "sorned": null}
+       {"period": "2018 Q4", "licensed": 3407, "sorned": 660},
+       {"period": "2017 Q4", "licensed": 3246, "sorned": 661},
+       {"period": "2016 Q4", "licensed": 3171, "sorned": 676},
+       {"period": "2015 Q4", "licensed": 3155, "sorned": 681},
+       {"period": "2014 Q4", "licensed": 2726, "sorned": 620},
+       {"period": "2013 Q4", "licensed": 2245, "sorned": null},
+       {"period": "2012 Q4", "licensed": 1289, "sorned": null}
   ];
   Morris.Line({
     element: 'hero-graph',
@@ -35,16 +35,16 @@ $(function () {
   Morris.Area({
     element: 'hero-area',
     data: [
-      {period: '2010 Q1', iphone: 2666, ipad: null, itouch: 2647},
-      {period: '2010 Q2', iphone: 2778, ipad: 2294, itouch: 2441},
-      {period: '2010 Q3', iphone: 4912, ipad: 1969, itouch: 2501},
-      {period: '2010 Q4', iphone: 3767, ipad: 3597, itouch: 5689},
-      {period: '2011 Q1', iphone: 6810, ipad: 1914, itouch: 2293},
-      {period: '2011 Q2', iphone: 5670, ipad: 4293, itouch: 1881},
-      {period: '2011 Q3', iphone: 4820, ipad: 3795, itouch: 1588},
-      {period: '2011 Q4', iphone: 15073, ipad: 5967, itouch: 5175},
-      {period: '2012 Q1', iphone: 10687, ipad: 4460, itouch: 2028},
-      {period: '2012 Q2', iphone: 8432, ipad: 5713, itouch: 1791}
+      {period: '2016 Q1', iphone: 2666, ipad: null, itouch: 2647},
+      {period: '2016 Q2', iphone: 2778, ipad: 2294, itouch: 2441},
+      {period: '2016 Q3', iphone: 4912, ipad: 1969, itouch: 2501},
+      {period: '2016 Q4', iphone: 3767, ipad: 3597, itouch: 5689},
+      {period: '2017 Q1', iphone: 6810, ipad: 1914, itouch: 2293},
+      {period: '2017 Q2', iphone: 5670, ipad: 4293, itouch: 1881},
+      {period: '2017 Q3', iphone: 4820, ipad: 3795, itouch: 1588},
+      {period: '2017 Q4', iphone: 15073, ipad: 5967, itouch: 5175},
+      {period: '2018 Q1', iphone: 10687, ipad: 4460, itouch: 2028},
+      {period: '2018 Q2', iphone: 8432, ipad: 5713, itouch: 1791}
     ],
     xkey: 'period',
     ykeys: ['iphone', 'ipad', 'itouch'],
@@ -56,12 +56,12 @@ $(function () {
   Morris.Bar({
     element: 'hero-bar',
     data: [
-      {device: 'iPhone', geekbench: 136},
-      {device: 'iPhone 3G', geekbench: 137},
-      {device: 'iPhone 3GS', geekbench: 275},
-      {device: 'iPhone 4', geekbench: 380},
-      {device: 'iPhone 4S', geekbench: 655},
-      {device: 'iPhone 5', geekbench: 1571}
+      {device: 'iPhone 6', geekbench: 136},
+      {device: 'iPhone 7', geekbench: 137},
+      {device: 'iPhone 7S', geekbench: 275},
+      {device: 'iPhone 8', geekbench: 380},
+      {device: 'iPhone 8 Plus', geekbench: 655},
+      {device: 'iPhone X', geekbench: 1571}
     ],
     xkey: 'device',
     ykeys: ['geekbench'],
@@ -77,11 +77,11 @@ $(function () {
     ykeys: ['value'],
     labels: ['Value'],
     data: [
-      {year: '2008', value: 20},
-      {year: '2009', value: 10},
-      {year: '2010', value: 5},
-      {year: '2011', value: 5},
-      {year: '2012', value: 20}
+      {year: '2014', value: 20},
+      {year: '2015', value: 10},
+      {year: '2016', value: 5},
+      {year: '2017', value: 5},
+      {year: '2018', value: 20}
     ]
   });
 
